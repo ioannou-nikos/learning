0856855deb3f021f5d9c4b375f4148636ef130d9
Nikolaos Ioannou <ioannou.nikos@gmail.com>
2017-07-30 23:16:55 +0300

    New analysis projects added from Makis

---
+++ django_project/blog/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.conf import settings
+import datetime
+
+
+def index(request):
+    return HttpResponse("Hello Django")
+
+
+def today_is(request):
+    now = datetime.datetime.now()
+    return render(request, 'blog/datetime.html', {
+        'now': now,
+        'template_name': 'blog/nav.html',
+        'base_dir': settings.BASE_DIR})
