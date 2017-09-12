e5f95a882a31082c0c77925b6ea5d3ab1fb14af9
Nikolaos Ioannou <ioannou.nikos@gmail.com>
2017-09-13 00:17:12 +0300

    Merge branch 'master' of https://github.com/ioannou-nikos/learning
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- textAnalytics/scratch.py
+++ textAnalytics/scratch.py
@@ -1,6 +1,11 @@
 # -*- coding:utf-8 -*-
-# This file will be used as a scratch file for writing snippets for the
-# book text analytics with python.
+
+"""
+Author: Nikos Ioannou
+This file will be used as a scratch file for writing snippets for the
+book text analytics with python.
+THis material is ment to be the starting point of my research.
+"""
 
 import pandas as pd 
 import matplotlib.pyplot as plt 
