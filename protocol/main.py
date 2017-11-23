e9fa27a7b37807746865c324aa65d3d351056c11
Nikolaos Ioannou <ioannou.nikos@gmail.com>
2017-11-24 00:49:00 +0200

    Protocol -> Modifications to main.py and analyze_protokola.ipynb. Now works with all records.

--- protocol/main.py
+++ protocol/main.py
@@ -7,6 +7,26 @@
 import pandas as pd
 
 
+def get_enotita_code(text):
+    """
+    Στη συνάρτηση αυτή από ένα κείμενο επιστρέφουμε τον κωδικό της ενότητας.
+    """
+    key_words = ["μαθία", "εσσαλονί", "ιλκίς", "έλλας", "ιερίας", "ερρών",
+                 "αλκιδικής"]
+    for en in key_words:
+        if en in text:
+            return key_words.index(en)
+    return -1
+
+
+def get_enotita_codes_from_series(user_col):
+    """
+    Πάρε τη στήλη user και δημιούργησε μια σειρά από κωδικούς
+    """
+    codes = [get_enotita_code(usr) for usr in user_col]
+    return codes
+
+
 def read_data_file(skip_footer=0):
     col_names = ['ap', 'user', 'in_subject', 'out_subject', 'in_date',
                  'out_date', 'diff']
